13b79b9c3577b6e0bd73c1ef08f56cac4206ff99
Dennis Eisold <de@itstall.de>
2021-05-31 00:28:55 +0200

    First commit

---
+++ Tileset_Sipgate_Office.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Tileset_Sipgate_Office" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="assets/Tileset_Sipgate_Office.png" width="480" height="480"/>
+</tileset>
---
+++ assets/Tileset_Sipgate_Office_rotated.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Tileset_Sipgate_Office_rotated" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="Tileset_Sipgate_Office_rotated.png" width="480" height="480"/>
+</tileset>
